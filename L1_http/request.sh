89b5983fbd132b263d4dda37b371d698e702c6d3
Ruslan <noname0263@gmail.com>
2026-02-26 07:23:56 +0300

    init

---
+++ L1_http/request.sh
@@ -0,0 +1,7 @@
+curl -X GET "http://109.167.241.225:8001/http_example/give_me_five?student=33&wday=5" \
+     -H "REQUEST_AGENT: ITMO student" \
+     -H "COURSE: Net Protocols" \
+     -H "User-Agent:" \
+     -H "Accept:" \
+     -H "Accept-Encoding:" \
+     --http1.1
